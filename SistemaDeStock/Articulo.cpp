b93b188e7fa725b7b39645227ce0cc1873d2af86
Yorgenis Martinez <yorgenis.0419@gmail.com>
2023-06-14 12:41:38 -0300

    Se creo la class articulo

---
+++ SistemaDeStock/Articulo.cpp
@@ -0,0 +1,204 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+#include "Articulo.h"
+
+void Articulo::setID(int ID)
+{
+    ArchivoArticulo CanArt;
+    _ID =ID;
+}
+void Articulo::setCategoria(string Categoria)
+{
+    strcpy(_Categoria, Categoria.c_str());
+}
+void Articulo::setModelo(string Modelo)
+{
+    strcpy(_Modelo, Modelo.c_str());
+}
+void Articulo::setTipoDeMaterial(string TipoDeMaterial)
+{
+    strcpy(_TipoDeMaterial, TipoDeMaterial.c_str());
+}
+void Articulo::setCapacidad(int Capacidad)
+{
+    _Capacidad = Capacidad;
+}
+void Articulo::setDiametro(int Diametro)
+{
+    _Diametro = Diametro;
+}
+void Articulo::setStock(int Stock)
+{
+    if(Stock > 0){
+    _Stock += Stock;
+    }
+}
+void Articulo::descontarStock(int stock){
+    if(_Stock >= stock){
+        _Stock -= stock;
+    }else{
+        cout << "Cantidad ingresada es mayor al stock disponible"<< endl;
+        cout << "Ingrese una cantidad menor o igual al Stock disponible"<< endl;
+        cout << "El stock disponible es: " << _Stock << endl;
+    }
+}
+void Articulo::setEstado(bool Estado)
+{
+    _Estado = Estado;
+}
+int Articulo::getID()
+{
+    return _ID;
+}
+string Articulo::getCategoria()
+{
+    return _Categoria;
+}
+string Articulo::getModelo()
+{
+    return _Modelo;
+}
+string Articulo::getTipoDeMaterial()
+{
+    return _TipoDeMaterial;
+}
+int Articulo::getCapacidad()
+{
+    return _Capacidad;
+}
+int Articulo::getDiametro()
+{
+    return _Diametro;
+}
+int Articulo::getStock()
+{
+    return _Stock;
+}
+bool Articulo::getEstado()
+{
+    return _Estado;
+}
+
+void Articulo::Cargar()
+{
+    ArchivoArticulo reg;
+    int validar=0;
+    cout << "Ingresar Categoria: ";
+    cin >> _Categoria;
+    while(validar==0)
+    {
+        cout << "Ingresar Modelo: ";
+        cin >> _Modelo;
+        if(reg.buscar1(_Modelo))
+        {
+            cout << "este modelo ya existe"<<endl;
+        }
+        else
+        {
+            validar=1;
+        }
+    }
+    cout << "Ingresar Tipo de Material: ";
+    cin >> _TipoDeMaterial;
+    cout << "Ingresar Capacidad: ";
+    cin >> _Capacidad;
+    cout << "Ingresar Diametro: ";
+    cin >> _Diametro;
+    cout << "Ingresar Stock: ";
+    cin >> _Stock;
+    _Estado = true;
+}
+void Articulo::Mostrar(int Aumentar)
+{
+    if(_Estado == true)
+    {
+       /* rlutil::locate(2,Aumentar);
+        cout << "|";
+        rlutil::locate(4,Aumentar);
+        cout << _ID;
+        rlutil::locate(7,Aumentar);
+        cout << "|";
+        rlutil::locate(10,Aumentar);
+        cout << _Categoria;
+        rlutil::locate(19,Aumentar);
+        cout << "|";
+        rlutil::locate(22,Aumentar);
+        cout << _Modelo;
+        rlutil::locate(28,Aumentar);
+        cout << "|";
+        rlutil::locate(31,Aumentar);
+        cout << _TipoDeMaterial;
+        rlutil::locate(39,Aumentar);
+        cout << "|";
+        rlutil::locate(43,Aumentar);
+        cout << _Capacidad;
+        rlutil::locate(51,Aumentar);
+        cout << "|";
+        rlutil::locate(54,Aumentar);
+        cout << _Diametro;
+        rlutil::locate(62,Aumentar);
+        cout << "|";
+        rlutil::locate(65,Aumentar);
+        cout << _Stock;
+        rlutil::locate(69,Aumentar);
+        cout << "|" << endl;*/
+    }
+}
+
+void Articulo::Mostrar2(int cant, int Aumentar)
+{
+    if(_Estado == true)
+    {
+       /*  rlutil::locate(2,Aumentar);
+        cout << "|";
+        rlutil::locate(4,Aumentar);
+        cout << _ID;
+        rlutil::locate(7,Aumentar);
+        cout << "|";
+        rlutil::locate(10,Aumentar);
+        cout << _Categoria;
+        rlutil::locate(19,Aumentar);
+        cout << "|";
+        rlutil::locate(22,Aumentar);
+        cout << _Modelo;
+        rlutil::locate(28,Aumentar);
+        cout << "|";
+        rlutil::locate(31,Aumentar);
+        cout << _TipoDeMaterial;
+        rlutil::locate(39,Aumentar);
+        cout << "|";
+        rlutil::locate(43,Aumentar);
+        cout << _Capacidad;
+        rlutil::locate(51,Aumentar);
+        cout << "|";
+        rlutil::locate(54,Aumentar);
+        cout << _Diametro;
+        rlutil::locate(62,Aumentar);
+        cout << "|";
+        rlutil::locate(65,Aumentar);
+        cout << cant;
+        rlutil::locate(72,Aumentar);
+        cout << "|" << endl;*/
+
+    }
+}
+void Articulo::Mostrar3(int Aumentar){
+    if(_Estado==true){
+
+      /*  rlutil::locate(2,Aumentar);
+        cout << "|";
+        rlutil::locate(4,Aumentar);
+        cout << _Categoria;
+        rlutil::locate(15,Aumentar);
+        cout << "|";
+        rlutil::locate(22,Aumentar);
+        cout << _Modelo;
+        rlutil::locate(28,Aumentar);
+        cout << "|"<< endl;*/
+    }
+}
+bool Articulo::modificar(){
+    cout << "Ingresar Stock: ";
+    cin >> _Stock;
+}
---
+++ SistemaDeStock/Articulo.h
@@ -0,0 +1,44 @@
+#ifndef ARTICULO_H
+#define ARTICULO_H
+
+#include <cstring>
+#include<string.h>
+using namespace std;
+class Articulo{
+private:
+    int _ID;
+    char _Categoria[50];
+    char _Modelo[50];
+    char _TipoDeMaterial[50];
+    int _Capacidad;
+    int _Diametro;
+    int _Stock;
+    bool _Estado;
+public:
+    void setID(int ID);
+    void setCategoria(string Categoria);
+    void setModelo(string Modelo);
+    void setTipoDeMaterial(string TipoDeMaterial);
+    void setCapacidad(int Capacidad);
+    void setDiametro(int Diametro);
+    void setStock(int Stock);
+    void descontarStock(int stock);
+    void setEstado(bool Estado);
+
+    int getID();
+    string getCategoria();
+    string getModelo();
+    string getTipoDeMaterial();
+    int getCapacidad();
+    int getDiametro();
+    int getStock();
+    bool getEstado();
+
+    void Cargar();
+    void Mostrar(int Aumentar);
+    void Mostrar2(int cant, int Aumentar);
+    void Mostrar3(int Aumentar);
+    bool modificar();
+};
+
+#endif // ARTICULO_H
