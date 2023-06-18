afa64c3e51f203db7b6351b30b54c362885b9d4e
Robielys <Robielys@users.noreply.github.com>
2023-06-17 21:19:28 -0300

    Clase DetalleDeMuestra
    esta es la clase que tiene el detalle de la muestra que nos comento el profe.

---
+++ SistemaDeStock/DetalleDeMuestra.cpp
@@ -0,0 +1,76 @@
+#include "DetalleDeMuestra.h"
+#include <string>
+#include <cstring>
+using namespace std;
+
+void DetalleDeMuestra::setIDmuestra(int idmuestra)
+{
+    _IDmuestra= idmuestra;
+}
+void DetalleDeMuestra::setIDproducto(int idproducto)
+{
+    _IDproducto= idproducto;
+}
+void  DetalleDeMuestra::setCategoria( string categoria)
+{
+    strcpy(_Categoria, categoria.c_str());
+}
+void  DetalleDeMuestra::setModelo(string modelo)
+{
+    strcpy(_Modelo, modelo.c_str());
+}
+void  DetalleDeMuestra::setTipoDeMaterial(string tipoDematerial)
+{
+    strcpy(_TipoDeMaterial, tipoDematerial.c_str());
+}
+void DetalleDeMuestra::setCapacidad(int capacidad)
+{
+    _Capacidad= capacidad;
+}
+void DetalleDeMuestra::setDiametro(int diametro)
+{
+    _Diametro= diametro;
+}
+void DetalleDeMuestra::setStock(int stock)
+{
+    _Stock= stock;
+}
+
+int DetalleDeMuestra::getIDmuestra()
+{
+    return _IDmuestra;
+}
+int DetalleDeMuestra::getIDproducto()
+{
+    return _IDproducto;
+}
+string DetalleDeMuestra::getCategoria()
+{
+    return _Categoria;
+}
+string DetalleDeMuestra::getModelo()
+{
+    return _Modelo;
+}
+string DetalleDeMuestra::getTipoDeMaterial()
+{
+    return _TipoDeMaterial;
+}
+
+int DetalleDeMuestra::getCapacidad()
+{
+    return _Capacidad;
+}
+int DetalleDeMuestra::getDiametro()
+{
+    return _Diametro;
+}
+int DetalleDeMuestra::getStock()
+{
+    return _Stock;
+}
+
+void guardar() {}
+void mostrar() {}
+void modificar() {}
+
---
+++ SistemaDeStock/DetalleDeMuestra.h
@@ -0,0 +1,46 @@
+#ifndef DETALLEDEMUESTRA_H
+#define DETALLEDEMUESTRA_H
+#include <string>
+using namespace std;
+
+
+class DetalleDeMuestra
+{
+
+    private:
+        int _IDmuestra;
+        int _IDproducto;
+        char _Categoria[9];
+        char _Modelo[5];
+        char _TipoDeMaterial[5];
+        int _Capacidad;
+        int _Diametro;
+        int _Stock;
+
+    public:
+
+        void setIDmuestra(int idmuestra);
+        void setIDproducto(int idproducto);
+        void  setCategoria( string categoria);
+        void  setModelo(string modelo);
+        void  setTipoDeMaterial(string tipoDematerial);
+        void setCapacidad(int capacidad);
+        void setDiametro(int diametro);
+        void setStock(int stock);
+
+        int getIDmuestra();
+        int getIDproducto();
+        string getCategoria();
+        string getModelo();
+        string getTipoDeMaterial();
+        int getCapacidad();
+        int getDiametro();
+        int getStock();
+
+        void guardar();
+        void mostrar();
+
+
+};
+
+#endif // DETALLEDEMUESTRA_H
